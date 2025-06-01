a76f624eeffd41656f49cbf96915b0c3ff62399f
saurabh jais <saurabhjais2000@gmail.com>
2025-06-01 14:39:45 +0530

    chnages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // allow external connections
+    allowedHosts: ['frontend-sj-cart-4.onrender.com'], // add your Render domain here
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,
+  },
 })
